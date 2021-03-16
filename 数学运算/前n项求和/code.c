9c93a957be8ecd7b11f03d077e6d2f7836c95dae
loganjin <58923238+loganjin@users.noreply.github.com>
2021-03-16 21:28:01 +0800

    Update code.c

--- 数学运算/前n项求和/code.c
+++ 数学运算/前n项求和/code.c
@@ -4,5 +4,21 @@
 
 int main(void)
 {
-  
+    int n;
+    int i;
+    int sum = 0;
+
+    int sign = 1;//symbol mark
+
+    scanf("%d", &n);
+
+    for (i = 1; i <= n;i++)
+    {
+        sum += i*sign;
+        sign = -sign;
+    }
+
+    printf("sum=%d\n", sum);
+
+    return 0;
 }
