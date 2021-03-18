7ae32f3003f32277c55c57fba7f702592b3a8838
loganjin <58923238+loganjin@users.noreply.github.com>
2021-03-18 15:51:17 +0800

    Update code.c

--- 格式/下三角的表示/code.c
+++ 格式/下三角的表示/code.c
@@ -1,1 +1,24 @@
+//i、j的相对顺序
+//左右对齐的概念
 
+#include<stdio.h>
+
+int main(void)
+{
+    int N;
+    scanf("%d",&N);
+    
+    int i;
+    int j;
+  
+    for(j=1;j<=N;j++)
+    {
+        for(i=1;i<=j;i++)//必须到j
+        {
+            printf("%d*%d=%-4d",i,j,i*j);//i、j的顺序很重要
+        }
+        printf("\n");//内层循环外进行换行
+    }
+    
+    return 0;
+}
